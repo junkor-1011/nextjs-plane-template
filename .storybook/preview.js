90dfb4db786806fb6e75553a5622f4e7b5356a43
junkor-1011 <tomoyukizxy@gmail.com>
2022-09-19 22:49:05 +0900

    install msw (& addon)

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,13 @@
 import * as NextImage from 'next/image';
+import { initialize, mswDecorator } from 'msw-storybook-addon';
 
 import '../src/styles/globals.css';
+
+// Initialize MSW
+initialize();
+
+// Provide the MSW addon decorator globally
+export const decorators = [mswDecorator];
 
 const OriginalNextImage = NextImage.default;
 
